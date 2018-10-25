397209a992df85707772571b166d3f7af7c392e3
lvyi <1257620619@qq.com>
2018-10-25 17:38:44 +0800

    第一次提交

---
+++ src/App.vue
@@ -0,0 +1,41 @@
+<template>
+  <div id="app">
+    <child :logo="logoMsg"></child>
+    <div>
+      <h1>这</h1>
+    </div>
+    <child :message="parentMsg" @parentData="showMsgFromChild"></child>
+    <preview></preview>
+  </div>
+</template>
+
+<script>
+  import child from './components/Child';
+  import preview from './components/preview';
+  export default {
+    data () {
+      return {
+        logoMsg:'hello,parent',
+        parentMsg:'hello,child'
+
+      }
+    },
+    components: {
+      child,
+      preview
+    },
+    methods:{
+      showMsgFromChild (data) {
+        console.log(data);
+      }
+    },
+    created () {
+
+    }
+
+  }
+</script>
+
+<style scoped>
+
+</style>
---
+++ src/components/Child.vue
@@ -0,0 +1,40 @@
+<template>
+    <div class="header">
+        <div id="logo">{{logo}}</div>
+      <ul class="nav">
+        <li v-for="nav in navs">{{nav.li}}</li>
+      </ul>
+      <div>
+        <h1>子组件部分</h1>
+        <!--<p>{{message}}</p>-->
+        <button @click="sendMsgToParent">向父组件传值</button>
+      </div>
+    </div>
+</template>
+
+<script>
+    export default {
+      name:'Child',
+      data () {
+        return {
+          username:'',
+          navs:[
+            {li:'home'},
+            {li:'abouts'},
+            {li:'contact'},
+            {li:'map'}
+            ]
+        }
+      },
+      props:['logo'],
+      methods:{
+        sendMsgToParent () {
+          this.$emit("parentData","this is message is from child")
+        }
+      }
+    }
+</script>
+
+<style scoped>
+
+</style>
